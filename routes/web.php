910fa077458aef34864f3152492ce3d7dbd1605a
Geovany Marroquin <gemarroquin.dev@gmail.com>
2024-12-15 09:55:46 -0600

    ✨Autenticacion

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+\Illuminate\Support\Facades\Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
