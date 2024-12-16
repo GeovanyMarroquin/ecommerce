95d9dba039b2ae1291f2a30380cf05736057d543
Geovany Marroquin <gemarroquin.dev@gmail.com>
2024-12-15 18:07:35 -0600

    ✨ Agregando plantilla

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+
+import '../admin-template/vendors/perfect-scrollbar/perfect-scrollbar.min.js'
+import '../admin-template/vendors/apexcharts/apexcharts.js'
+import '../admin-template/js/pages/dashboard.js'
+import '../admin-template/js/main.js'
