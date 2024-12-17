52f58a84f05f90b2133bc05066fe6238c08f684b
Geovany Marroquin <pablo.marroquin@pnc.gob.sv>
2024-12-17 16:59:39 -0600

    ✨Cambios en tablas y eventos

--- resources/js/usuarios/list.js
+++ resources/js/usuarios/list.js
@@ -1,30 +1,56 @@
+import TableComponent from "../utils/table-component.js";
 import {obtenerUsuarios} from "./user-service.js";
-import {Grid, html} from "gridjs";
+import {html} from "gridjs";
+import {Modal} from "bootstrap";
 
 const columnsUsersAll = [
-    {name: "Id"},
-    {name: "Nombre"},
-    {name: "Correo"},
+    {id: "id", name: "Id"},
+    {id: "name", name: "Nombre Completo"},
+    {id: "email", name: "Correo"},
     {
-        name: "Acciones", formatter: (cell, row) => {
+        name: "Acciones",
+        formatter: (cell, row) => {
             return html(`
-                <a type="button" class="btn btn-primary btn-sm" href="${row.cells[3].data}">Editar</a>
+                <a type="button" class="btn btn-warning editUser" data-href="${row.cells[3].data}">
+                    <i class="bi bi-pencil"></i>
+                    Editar
+                </a>
             `);
-        }
+        },
     },
 ];
+
+
+const btnAddUser = document.querySelector("#btnAddUser");
 
 export async function init() {
     // aca es necesario agregar todos los eventos de los inputs
     // se puede modularizar en diferentes archivos
-    const usuarios = await obtenerUsuarios();
-    const frm = usuarios.map(x => Object.keys(x).map(y => x[y]));
-    console.log({frm})
-    console.log({usuarios})
-    new Grid({
-        //columns: ["Id", "Name", "Email", "last updated"],
-        columns: columnsUsersAll,
-        data: frm
-    }).render(document.getElementById("tableAllUsers"));
+    const users = await obtenerUsuarios();
+    const frm = users.map(x => Object.keys(x).map(y => x[y]));
 
+    const tableUsersInstance = new TableComponent("tableAllUsers", columnsUsersAll, frm);
+
+    // Manejo de eventos para los botones
+    tableUsersInstance.getTableElement().addEventListener("click", (event) => {
+        if (event.target.classList.contains("editUser")) {
+            const route = event.target.getAttribute("data-href");
+            editarUsuario(route);
+        }
+    });
+
+    btnAddUser.addEventListener("click", () => {
+        addNewUser();
+    });
+}
+
+const editarUsuario = (route) => {
+
+}
+
+const addNewUser = () => {
+    const modal = new Modal('#modalAggNewUser', {
+        keyboard: false
+    });
+    modal.show();
 }
